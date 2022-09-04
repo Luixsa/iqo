bdab6221e4d52c8b4ab513c0cbea09d3af6bcd94
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-04 09:25:14 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
     { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
     { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://www.miyano.ml' }
+    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Miyano', href: 'https://www.miyano.ml' }
 ]
 
 import Link from 'next/link';
