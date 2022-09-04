3ad8585ca6326731e6c14e18d8f1022cb740b663
Hex010101 <112714027+Hex010101@users.noreply.github.com>
2022-09-04 11:38:05 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -44,7 +44,7 @@ I am a professional editor and software developer. I have been professionally in
           </div>
       </div>
 
-
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ajih-nnIS14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
    
 
 
