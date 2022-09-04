ad87002628829e8b5485718ec9d6881b66a2f60c
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-04 09:14:01 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,17 +2,17 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
-      name: "Code Share",
-      description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
-      link: "https://codeshare.me",
+      image: "https://cdn.discordapp.com/attachments/997187785154908311/1015831633796018286/miyanom.jpg",
+      name: "Miyano Bot",
+      description: "Are you ready to throw the best parties with Miyano, the best and most up-to-date bot?",
+      link: "https://discord.gg/J69KSqvGeq",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "Youtube Channel",
-      description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
-      link: "https://youtube.com/UmutBayraktarYT",
+      image: "https://cdn.discordapp.com/attachments/997187785154908311/1015831633796018286/miyanom.jpg",
+      name: "Miyano Bot",
+      description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.",
+      link: "https://discord.gg/J69KSqvGeq",
     }
   ];
   res.status(200).json(data);
